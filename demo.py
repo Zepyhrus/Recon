81b15ef6455497d7e620ae99bca7861477af1414
Zepyhrus <sherkfung@gmail.com>
2022-06-04 16:45:39 +0800

    add Api

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 
 from ChishineApi import CamChi
 
@@ -9,11 +11,18 @@ from ChishineApi import CamChi
 
 if __name__ == '__main__':
   cam_ip = '192.168.2.7'
+  cnt = 0
 
   with CamChi(cam_ip) as cam:
     while True:
       pc, img = cam.grab()
 
       cv2.imshow('_', img)
-      if cv2.waitKey(1) == 27:
+      cnt += 1
+
+      cv2.imwrite(f'data/{cnt}.png', img)
+      np.save(f'data/{cnt}', pc)
+
+
+      if cv2.waitKey(1) == 27 or cnt >= 100:
         break
